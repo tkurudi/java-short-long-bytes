cf89f858b32a31d8b52f54f24f0b9b6f4c7c7c6e
tkurudi <t.kurudi209@gmail.com>
2019-09-04 19:34:09 +1200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myMinValue = Integer.MIN_VALUE;
+        int myMaxValue = Integer.MAX_VALUE;
+
+        System.out.println("Int min value  " + myMinValue);
+        System.out.println("Int max value  " + myMaxValue);
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+
+        System.out.println("min byte " + myMinByteValue);
+        System.out.println("max byte " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+
+        System.out.println("min Short " + myMinShortValue);
+        System.out.println("max Short " + myMaxShortValue);
+
+        long myLongValue = 100L;
+
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+
+        System.out.println("min long " + myMinLongValue);
+        System.out.println("max long " + myMaxLongValue);
+    }
+    }
+
